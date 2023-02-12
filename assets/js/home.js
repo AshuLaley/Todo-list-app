5748dd869178af4f611ae450ccc81d56876c9c4c
Ashutosh <ashulaley@gmail.com>
2023-02-12 22:36:12 +0530

    Project Completed

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,2 +1,23 @@
-console.log("My script is loaded");
+var displayCategory = document.getElementById("display-category");
+let del_btn = document.getElementById('delete');
+
+// if(i.category=="PERSONAL")
+// {document.getElementById("display-category").style.backgroundColor = "#279f12" ; }
+
+
+
+del_btn.addEventListener('click',function(){
+    //get the array of checkbox elements that are checked
+    let checkedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+
+    //get the array of task IDs from the value attribute of the checked checkboxes
+    let taskIdArray = Array.from(checkedCheckboxes).map(checkbox => checkbox.value);
+
+    //create the URL for the GET request
+    let url = '/delete-tasks?id_array[]=' + taskIdArray.join('&id_array[]=');
+
+    console.log(url);
+    //send the GET request to the server
+    window.location.href = url;
+});
 
---
+++ models/tasks.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    taskname:{
+        type: String,
+        required: true
+    },
+
+    category:{
+        type: String,
+        required: false
+    },
+
+    deadline:{
+        type:String,
+        required: false
+    }
+})
+
+const Task = mongoose.model('Task',taskSchema);
+
+module.exports = Task;
